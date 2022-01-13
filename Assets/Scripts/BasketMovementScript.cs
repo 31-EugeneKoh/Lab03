c6a2e121f3e8fb78e34fd6409116e0d396bf5bd2
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-01-13 10:18:11 +0800

    Added Win and Lose Conditions
    Added WinScene and LoseScene, along with the basket's ability to register the falling items.

--- Assets/Scripts/BasketMovementScript.cs
+++ Assets/Scripts/BasketMovementScript.cs
@@ -1,32 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
-   
+    private int healthyCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ItemsCollected();
     }
 
     // Update is called once per frame
     void Update()
     {
-
       float horizontalInput = Input.GetAxis("Horizontal");
 
       transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
-
-        
-       
-
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Unhealthy")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
 
+        if (other.gameObject.tag == "Healthy")
+        {
+            Destroy(other.gameObject);
+            healthyCount++;
+        }
+    }
 
-    
-
+    void ItemsCollected()
+    {
+        if (healthyCount >= 5)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+    }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,36 +9,24 @@ public class Spawner : MonoBehaviour
     private float spawndelay=1;
     private int randomInt;
     float positionx;
-   
-
-    
 
     // Start is called before the first frame update
     void Start()
-    {
-      
+    {      
         InvokeRepeating("spawnobject", spawntime, spawndelay);
-
-     
-
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
        // spawntime = Random.Range(1, 3);
     }
 
     void spawnobject()
-    {
-       
-        
+    {       
         randomInt = Random.Range(0, spawnee.Length);
         positionx = Random.Range(8f,-8f);
         this.transform.position = new Vector3(positionx,transform.position.y , transform.position.z);
         Instantiate(spawnee[randomInt], transform.position, transform.rotation);
-   
-
     }
 }
