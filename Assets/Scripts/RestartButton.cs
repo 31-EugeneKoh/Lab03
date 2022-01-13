474eab52c41e06a713420e1ef7dfaa998c25d216
31-EugeneKoh <OverpoweringPower@gmail.com>
2022-01-13 10:33:46 +0800

    Implemented Restart Buttons
    Implemented Restart Buttons for the Game Win and Game Lose Scenes.

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnRestart()
     {
-        
+        SceneManager.LoadScene("GamePlay_Level 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
